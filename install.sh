9b5bdd6355d6ce32049bc33953e485c1172e9856
Giulio Collura <giulio.collura@gmail.com>
2015-06-05 09:13:04 +0200

    use splitbelow, splitright in vim. add eclimrc

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
+echo "Updating submodules"
+
+git submodule update --init
+
 echo "Creating symlinks"
 
 set -x
 
 ln -s $PWD/tmux.conf ~/.tmux.conf
 ln -s $PWD/tmux ~/.tmux
-ln -s $PWD/irssi ~/.irssi
+# ln -s $PWD/irssi ~/.irssi
 ln -s $PWD/vim ~/.vim
 ln -s $PWD/xdefaults/Xresources ~/.Xresources
 # ln -s $PWD/xdefaults/Xcolors.atelier ~/.config
+ln -s $PWD/eclimrc ~/.eclimrc
